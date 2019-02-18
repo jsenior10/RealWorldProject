d2e072032cc1f327abc8b79863d06b652f4993fa
Jacob <jacobsenior4@gmail.com>
2019-02-18 02:22:32 +0000

    Draws a box around the largest item of a certain colour, improved documentation
    Signed-off-by: Jacob <jacobsenior4@gmail.com>

--- MachineVision/findColour.py
+++ MachineVision/findColour.py
@@ -2,8 +2,11 @@ import cv2
 import numpy as np
 
 
-def findColour(filename, colour):
-    """Function that takes path of an image and outputs a new file highlighting said colour."""
+def findColour(filename, colour) -> object:
+    """Function that takes path of an image and outputs a new file highlighting said colour.
+    :param filename: name of image you want to process
+    :param colour: the colour you want to extract (green/red)
+    """
 
     image = cv2.imread(filename, 1)  # gcolour
     blurred_image = cv2.GaussianBlur(image, (5, 5), 0)  # blurred to remove noise
@@ -12,8 +15,8 @@ def findColour(filename, colour):
     # RED COLOUR BOUNDARIES
     red_lower = np.array([0, 150, 0])  # pair of least red and most red on the hsv map
     red_upper = np.array([7, 255, 359])
-    wrap_around_lower = np.array([170, 150, 0])  # still need tweaking, doesnt pick up very light reds
-    wrap_around_upper = np.array([180, 255, 359])
+    wrap_around_lower = np.array([170, 150, 0])    # still need tweaking, doesnt pick up very light reds
+    wrap_around_upper = np.array([180, 255, 359])  # Could draw box around item to remedy this?
 
     # GREEN COLOUR BOUNDARIES
     green_lower = np.array([40, 60, 60])
@@ -23,21 +26,29 @@ def findColour(filename, colour):
         mask = cv2.inRange(hsv, red_lower, red_upper)
         additional_mask = cv2.inRange(hsv, wrap_around_lower, wrap_around_upper)
         mask += additional_mask
-        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL,
-                                       cv2.CHAIN_APPROX_NONE)  # finds the edges between white and black for the mask
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL,  # finds the edges between white and black for the mask
+                                       cv2.CHAIN_APPROX_SIMPLE)  # contours returns an array with every edge AFAIK
         contoured = cv2.drawContours(image, contours, -1, (255, 0, 0), 3)  # draws the edges
         cv2.imwrite('./Output/' + colour + '1.jpg', contoured)
 
     if colour == "green":
         mask = cv2.inRange(hsv, green_lower, green_upper)
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL,
-                                       cv2.CHAIN_APPROX_NONE)
+                                       cv2.CHAIN_APPROX_SIMPLE)
         contoured = cv2.drawContours(image, contours, -1, (255, 0, 0), 3)
         cv2.imwrite('./Output/' + colour + '1.jpg', contoured)
 
     output = cv2.bitwise_and(image, image, mask=mask)  # highlights our mask onto the original image
-
     cv2.imwrite('./Output/' + colour + '2.jpg', output)
+
+    if len(contours) != 0:  # check if there's anything in contours
+        cv2.drawContours(output, contours, -1, 255, 3)  # draws contours in blue
+
+        c = max(contours, key=cv2.contourArea)  # finds contour with largest area
+
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 2)  # draw rectangle around largest cone
+        cv2.imwrite('./Output/' + colour + 'LargestArea.jpg', output)
 
     for contour in contours:
         area = cv2.contourArea(
@@ -49,4 +60,4 @@ def findColour(filename, colour):
 
 
 if __name__ == "__main__":
-    findColour("./Assets/test2.jpg", "green")
+    findColour("./Assets/test3.jpg", "red")
