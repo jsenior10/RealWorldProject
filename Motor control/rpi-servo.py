7fed8f44b7d7175575312c9044795d7d51470c29
Ryan Yiu (yiur) <yiur@coventry.ac.uk>
2019-02-25 13:11:38 +0000

    Updated

--- Motor control/rpi-servo.py
+++ Motor control/rpi-servo.py
@@ -4,12 +4,12 @@ import time
 GPIO.setmode(GPIO.BCM)	#Sets the GPIO mode
 GPIO.setup(23,GPIO.OUT)	#Pin 23 to be used and data out
 pwm = GPIO.PWM(23,50)	#PWM mode for pin 23
-pwm.start(7)
+pwm.start(9.75)			#Steering is centered when program is running
 
 
 for i in range(0, 180):
     inputdegree=input("Input degree: ")	#Input for degree rotation
-    DC=1./18.*(inputdegree) + 2			#Duty Cycle Equation
+    DC=13/360*(inputdegree) + 6.5			#Degree to duty cycle conversion
     pwm.ChangeDutyCycle(DC)				
 
 pwm.stop()
