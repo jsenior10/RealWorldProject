4e9bf7685fb861d2a810d68c6c81fd10a507d0fd
sergiuharjau <47636643+sergiuharjau@users.noreply.github.com>
2019-03-14 12:05:21 +0000

    Refactored code. Added rough turning directions as well. Increased efficiency.

--- development/MachineVision/sketch.py
+++ development/MachineVision/sketch.py
@@ -1,58 +1,24 @@
 import time
 import RPi.GPIO as GPIO
-import time
 import cv2
 import io
 from picamera import PiCamera
-#import picamera.array
 import numpy as np
 from findColour import findColour
 
-def test():
-  stream = io.BytesIO()
-  data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-  image = cv2.imdecode(data,1)
-  findColour(image, False)
-  count = 0
-  if maskRed == None:
-    print("Didn't find red.")
-    #continue
-  exitProgram = False
-
-  for i in range(int(len(maskRed)*0.6), int(len(maskRed)), 15):
-    print("Counting pixels")
-    for k in range(80,240): #for performance reasons
-        pixel = maskRed[i][k]
-        if pixel == 255:
-            count += 1
-        if count > 150:
-            exitProgram = True
-            break
-    if exitProgram:
-      break
-    print("Didn't find red. Next frame")
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(16, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
-#stream = [io.BytesIO() for i in range(30)]
-#stream = io.BytesIO()
-
-steer = GPIO.PWM(16, 50)
 speed = GPIO.PWM(18, 50)
+
 print("initialising camera")
 start = time.time()
+
 camera = PiCamera()
 camera.resolution = (480, 240)
-#camera.framerate(30)
 camera.start_preview()
-end = time.time()
-print("camera initialised - time taken ")
-print(end - start)
 
-#if input("Direction? ") == "l":
-#	steer.start(15) #left
-#else:
-#	steer.start(1) #right
+end = time.time()
+print("camera initialised - time taken ", end-start)
 
 while True:
 	userInput = input("Speed: ")
@@ -61,97 +27,65 @@ while True:
 	speed.start(float(userInput))
 
 try:
-  #print("In the future well try finding red here")
 	while True:
-			#camera.resolution = (720, 240)
-			#camera.start_preview()
-			#time.sleep(2)
 		stream = io.BytesIO()
-		#print("Taking picture")
-		#input("Enter to take picture")
-		#camera.start_preview()
 		start = time.time()
 		camera.capture(stream,format='jpeg', use_video_port=True)
-		#camera.capture('./Output/cam.jpg', format='jpeg')
 		end = time.time()
-		#print("We have taken a picture")
-		#print("Amount of time to take picture ", end - start)
+		print("Amount of time to take picture ", end - start)
 		data = np.fromstring(stream.getvalue(), dtype=np.uint8)
 
 		image = cv2.imdecode(data,1)
 		maskRed, maskYellow = findColour(image, False)
 
-		count = 0
-		#if maskRed == None:
-			#print("Didn't find red.")
-			#continue
+		redCount = 0
+		yellowCount = 0 
 		foundRed = False 
 		foundYellow = False 
-		start = time.time()
-		#print("Lenght of maskRed, ", len(maskRed) )
-		#input()
-		#print(int(len(maskRed)*0.87), int(len(maskRed)*0.9))
-		redPositions = {}
+
 		for i in range(int(len(maskRed)*0.892), int(len(maskRed)*0.9)):
-			#print("Counting pixels ", len(maskRed[i]))
-			#redPositions[i] = [None, None] 
-			for k in range(0,int(len(maskRed[i])/2)): #for performance reasons
-				#print(pixel, end=" ")
-				if maskRed[i][k] == 255:
-					if count == 0:
+			for k in range(0,int(len(maskRed[i])/2)): #looks at the left pixels 
+				if !foundRed and maskRed[i][k]: #faster than == 255
+					if !redCount : #when count is 0 
 						positionRed = k
-					#if redPositions[i][0] == None:
-					#	redPositions[i][0] = k
-					#redPositions[i][1] = k
-					lastRed = k
-					count += 1
-				#print("Current count: ", count)
-			if count > 25:
-				#print(redPositions)
-				#keys = list(redPositions.keys())
-				sizeRed = lastRed - positionRed #redPositions[max(keys)][1] - redPositions[min(keys)][0]
-				#position = #rdPositions[min(keys)][0]
-				#print("Red size(px): ", sizeRed)
+					redCount += 1
+	   	   	  
+			for k in reversed(range(int(len(maskYellow[i])/2), len(maskYellow[i]))): #right of image 
+	   	   	     #reversed looks at the pixels right to left 
+				if !foundYellow and maskYellow[i][k]: #faster than == 255 
+					if !yellowCount: #when count == 0 
+						positionYellow = k
+					yellowCount += 1   
+
+			if redCount > 25:
 				print("Red at position: ", positionRed)
 				#print("Red pixel count: ", count)
 				foundRed = True
-				break
-		count = 0
-		#print(maskYellow)
-		#input("")
-		for i in range(int(len(maskYellow)*0.892), int(len(maskYellow)*0.9)):
-			#print("\nCounting yellow pixels ", len(maskRed[i]))
-			#redPositions[i] = [None, None] 
-			for k in range(int(len(maskYellow[i])/2), len(maskYellow[i])): #for performance reasons
-				#print(maskYellow[i][k], end=" ")
-				if maskYellow[i][k] == 255:
-					if count == 0:
-						positionYellow = k
-					#if redPositions[i][0] == None:
-					#	redPositions[i][0] = k
-					#redPositions[i][1] = k
-					lastYellow = k
-					count += 1
-				#print("Current count: ", count)
-			if count > 25:
-				#print(redPositions)
-				#keys = list(redPositions.keys())
-				sizeYellow = lastYellow - positionYellow #redPositions[max(keys)][1] - redPositions[min(keys)][0]
-				#position = #redPositions[min(keys)][0]
-				#print("Yellow size(px): ", sizeYellow)
+
+			if yellowCount > 25:
 				print("Yellow at position: ", positionYellow)
 				#print("Yellow pixel count: ", count)
 				foundYellow = True
-				break
+
 		if foundRed and foundYellow:
-			print("\n\nWe are this close(px): ", 480-positionYellow-positionRed)
-			if (positionRed == 480 - positionYellow):
-				break
+			direction = 480-positionYellow-positionRed
+			
+			print("\nWe are this close: ", direction)
+			if direction < 15 and direction > -15:
+				print("Dead on. Keep straight.")
+			elif direction > 100:
+				print("Turn left.")
+			elif direction < -100:
+				print("Turn right")
+			elif direction < -50:
+				print("Almost there! Tiny bit left!")
+			elif direction > 50: 
+				print("Almost there! Tiny bit right!")
+
 		end = time.time()
-		#print("Didn't find both. Next frame")
-		#print("Processing time: ", end - start)
+		print("Total time spent on frame: ", end - start)
 
 except(KeyboardInterrupt):
-  GPIO.cleanup()
+	GPIO.cleanup()
 
 GPIO.cleanup()
